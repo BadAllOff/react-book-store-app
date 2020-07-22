bfa16d9c5e98def8da4723fda1bdf6511639c473
Shamil <shamil.badalloffsh@gmail.com>
2020-07-22 14:05:29 +0200

    Add: author component

---
+++ src/Author.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Author extends React.Component {
+  render() {
+    console.log(this.props);
+    const { author } = this.props;
+
+    return (
+      <div className="card border-dark">
+        <img
+          class="card-img-top"
+          src={author.avatar}
+          alt="Card image cap"
+        ></img>
+        <div class="card-body">
+          <h5 class="card-title">{author.name}</h5>
+          <p class="card-text">{author.about}</p>
+          <p class="card-text">
+            <small class="text-muted">{author.email}</small>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Author;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom";
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
+import App from './App'
 
 ReactDOM.render(
-  <div>Hello, World</div>,
+  <App />,
   document.getElementById('root')
 );
