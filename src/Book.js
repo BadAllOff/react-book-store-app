81cd33baf6dff9d733004b840ca93f28492520aa
Shamil <shamil.badalloffsh@gmail.com>
2020-07-22 14:42:22 +0200

    Add: signuptobook component

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Author from './Author';
+import Signuptobook from './SIgnUpToBook'
 class Book extends React.Component {
   render() {
     const { book } = this.props;
     return (
       <div className="card-columns">
-        <div className="card">
+        <div className="card border-dark">
           <img
             src={book.cover_image}
             className="card-img-top"
@@ -20,28 +21,29 @@ class Book extends React.Component {
             <p className="pull-right text-right"> <span className="pull-right text-right">{book.author.name}</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Pages count</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.page_count}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">{book.page_count} pages</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Language</span></p>
             <p className="pull-right text-right"> <span className="pull-right text-right">{book.language}</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Progress</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.progress}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">{book.progress}%</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Minimum price</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.min_price}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">${book.min_price}</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Main price</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.main_price}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">${book.main_price}</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Total sum</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.total_sum}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">${book.total_sum}</span> </p>
             <hr/>
             <p className="card-text"><span className="text-primary">Expected revenue</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.expected_sum}</span> </p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">${book.expected_sum}</span> </p>
           </div>
         </div>
         <Author author={book.author} />
+        <Signuptobook book={book} />
       </div>
     );
   }
---
+++ src/SIgnUpToBook.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Signuptobook extends React.Component {
+  render() {
+    console.log(this.props);
+    const { book } = this.props;
+
+    return (
+      <div className="card border-dark">
+        <div className="card-body">
+    <p className="card-text">Sign up and get latest information about "{book.title}" </p>
+          <form>
+            <div className="form-group row">
+              <div className="col-sm-12">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <div className="col-sm-12">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Sign up
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signuptobook;
