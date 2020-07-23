d46d32411d0a83f793d0c5cb5ad526435fa2b511
Shamil <shamil.badalloffsh@gmail.com>
2020-07-23 15:16:12 +0200

    add: first test added with babel register and presets

--- src/Author.js
+++ src/Author.js
@@ -2,17 +2,16 @@ import React from "react";
 
 class Author extends React.Component {
   render() {
-    console.log(this.props);
     const { author } = this.props;
 
     return (
       <div className="card border-dark">
-        <img class="card-img-top" src={author.avatar} alt={author.name} />
-        <div class="card-body">
-          <h5 class="card-title">{author.name}</h5>
-          <p class="card-text">{author.about}</p>
-          <p class="card-text">
-            <small class="text-muted">{author.email}</small>
+        <img className="card-img-top" src={author.avatar} alt={author.name} />
+        <div className="card-body">
+          <h5 className="card-title">{author.name}</h5>
+          <p className="card-text">{author.about}</p>
+          <p className="card-text">
+            <small className="text-muted">{author.email}</small>
           </p>
         </div>
       </div>
