1808e9def8afc77368e79bb3bf1045973683a49d
Shamil <shamil.badalloffsh@gmail.com>
2020-07-25 13:58:35 +0200

    Add: Bookslist Component, bestseller badge, multiaply authors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import React from "react";
 
-import Book from "./Book";
-
-const author = {
-  name: "Mr. Great Author",
-  email: "greatAuthor@autors.com",
-  avatar:
-    "https://i.wpimg.pl/730x0/m.gadzetomania.pl/tumblr-kwh4eacbzu1qaptl6-a9d2590.jpg",
-  about:
-    "The greatest author of all times that have become as it by never publishing books",
-};
-
-const book = {
-  title: "Best book title",
-  description: "short description of book",
-  page_count: 200,
-  language: "Russian",
-  progress: 20,
-  cover_image:
-    "https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png",
-  author: author,
-  min_price: "99,99",
-  main_price: "1000000",
-  total_sum: "0.01",
-  expected_sum: "1000000000",
-};
+import BooksList from "./BooksList";
 
 class App extends React.Component {
   render() {
@@ -42,7 +18,7 @@ class App extends React.Component {
           </nav>
         </header>
         <main className="container" style={{ marginTop: "30px" }}>
-          <Book book={book} />
+          <BooksList books={this.props.books} />
         </main>
         <footer>
           <div className="container">
--- src/Book.js
+++ src/Book.js
@@ -2,8 +2,17 @@ import React from "react";
 import Author from './Author';
 import Signuptobook from './SIgnUpToBook'
 class Book extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
   render() {
-    const { book } = this.props;
+    const {book} = this.props;
+    const authors = book.authors.filter(function(a, index) {if (index < 3){ return a; }});
+    const isBestseller = book.subscribers_count > 50 ? true : false;
+
     return (
       <div className="card-columns">
         <div className="card border-dark">
@@ -13,12 +22,20 @@ class Book extends React.Component {
             alt={book.title}
           />
           <div className="card-body">
-            <h5 className="card-title text-uppercase">{book.title}</h5>
+            <h5 className="card-title text-uppercase">{book.title} 
+            {isBestseller && 
+            <>
+              <br />
+              <span className="badge badge-pill badge-warning">Bestseller!</span>
+            </>}
+            </h5>
             <p className="card-text "><span className="text-primary">Description</span></p>
             <p className="pull-right text-right"> <span className="pull-right text-right">{book.description}</span> </p>
             <hr/>
-            <p className="card-text "><span className="text-primary">Author</span></p>
-            <p className="pull-right text-right"> <span className="pull-right text-right">{book.author.name}</span> </p>
+            <p className="card-text "><span className="text-primary">Authors</span></p>
+            {book.authors.map(author => (        
+            <p key={author.id} className="pull-right text-right"> <span className="pull-right text-right">{author.name}</span> </p>
+            ))}
             <hr/>
             <p className="card-text"><span className="text-primary">Pages count</span></p>
             <p className="pull-right text-right"> <span className="pull-right text-right">{book.page_count} pages</span> </p>
@@ -40,9 +57,16 @@ class Book extends React.Component {
             <hr/>
             <p className="card-text"><span className="text-primary">Expected revenue</span></p>
             <p className="pull-right text-right"> <span className="pull-right text-right">${book.expected_sum}</span> </p>
+            <hr/>
+            <p className="card-text"><span className="text-primary">Subscribers</span></p>
+            <p className="pull-right text-right"> <span className="pull-right text-right">{book.subscribers_count}</span> </p>
           </div>
         </div>
-        <Author author={book.author} />
+        {
+          authors.map(author => (
+            <Author key={author.id} author={author} />
+          ))
+        }
         <Signuptobook book={book} />
       </div>
     );
