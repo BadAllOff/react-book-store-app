fbd869cf3413f9eeba032ef140bca842947066cf
Shamil <shamil.badalloffsh@gmail.com>
2020-07-26 14:21:49 +0200

    add: functionality to show all authors button

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -2,35 +2,50 @@ import React from "react";
 import AuthorCard from "./AuthorCard";
 
 class AuthorsList extends React.Component {
-  render() {
-    const { authors } = this.props;
+  constructor(props) {
+    super(props);
 
+    this.state = { showAllAuthors: false };
+  }
+
+  toggleShowAll() {
+    this.setState({ showAllAuthors: !this.state.showAllAuthors });
+  }
+
+  render() {
+    const showAllAuthors = this.state.showAllAuthors;
+    const { authors } = this.props;
+    const showBtnText = `Show all ` + authors.length + ` Authors`;
+    const hideBtnText = `Show anly 3 Authors`;
+
+    // возможно не самое элегантное решение но работает. поэтому жду комментариев по улучшению.
+    // например сменить логику отображения - рендерить всё и играться с классами visible\invisible
+    // ну или дождаться следующих уроков где буду соображать получше.
     const visibleAuthors = authors.filter(function (a, index) {
-      if (index < 3) {
+      if (showAllAuthors) {
+        return a;
+      } else if (index < 3) {
         return a;
       }
     });
-    const restAuthors = authors.filter(function (a, index) {
-      if (index > 2) {
-        return a;
-      }
-    });
-    // console.log(restAuthors.length > 0);
 
     return (
       <>
-        {visibleAuthors.map((author) => (
-          <AuthorCard key={author.id} author={author} />
-        ))}
-        {restAuthors.length > 0 && (
+        {authors.length > 3 && (
           <div className="card border-primary">
             <div className="card-body">
-              <button className="btn btn-block btn-primary">
-                Show all {authors.length} Authors
+              <button
+                className="btn btn-block btn-primary"
+                onClick={() => this.toggleShowAll()}
+              >
+                {showAllAuthors ? hideBtnText : showBtnText}
               </button>
             </div>
           </div>
         )}
+        {visibleAuthors.map((author, index) => (
+          <AuthorCard key={author.id} author={author} className="invisible" />
+        ))}
       </>
     );
   }
