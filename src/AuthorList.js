8bb3d6198f8193fe554cdab29e2c6da42dedac7f
Shamil <shamil.badalloffsh@gmail.com>
2020-07-29 12:15:52 +0200

    add: show authors btn component

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -1,50 +1,17 @@
 import React from "react";
 import AuthorCard from "./AuthorCard";
+import ShowAuthorsBtn from "./ShowAuthorsBtn";
 
 class AuthorsList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { showAllAuthors: false };
+    this.toggleShowAll = this.toggleShowAll.bind(this);
   }
 
   toggleShowAll() {
     this.setState({ showAllAuthors: !this.state.showAllAuthors });
-  }
-
-  // в вспомогательных функциях как лучше -
-  // явно указывать в аргуметах какие параметры использует функция showBtn(authors, showAllAuthors)
-  // или же брать эти параметры из состояния внутри функции? this.state.showAllAuthors
-  // понимаю что в таком маленьком компоненте конечно это не имеет значения и возможно тема холиварная
-  // Мне например удобно когда в аргументах указаны все параметы используемые внутри.
-  // Таким образом всё становиться наглядно уже в "заголовке" функции
-  showBtn(authors, showAllAuthors) {
-    return (
-      <>
-        {authors.length > 3 && (
-          <div className="card border-primary">
-            <div className="card-body">
-              <button
-                className="btn btn-block btn-primary"
-                onClick={() => this.toggleShowAll()}
-              >
-                {this.toggleBtnText(authors, showAllAuthors)}
-              </button>
-            </div>
-          </div>
-        )}
-      </>
-    );
-  }
-
-  toggleBtnText(authors, showAllAuthors) {
-    return (
-      <>
-        {showAllAuthors
-          ? `Show only 3 Authors`
-          : `Show all ${authors.length} Authors`}
-      </>
-    );
   }
 
   render() {
@@ -52,12 +19,15 @@ class AuthorsList extends React.Component {
     const showAllAuthors = this.state.showAllAuthors;
     const authorsToShow = showAllAuthors ? authors : authors.slice(0, 3);
 
-    // убрал логику из финального рендера, если это имеет значение :)
     return (
       <>
-        {this.showBtn(authors, showAllAuthors)}
-        {authorsToShow.map((author, index) => (
-          <AuthorCard key={author.id} author={author} className="invisible" />
+        <ShowAuthorsBtn
+          authors={authors}
+          showAllAuthors={showAllAuthors}
+          toggleShowAll={this.toggleShowAll}
+        />
+        {authorsToShow.map((author) => (
+          <AuthorCard key={author.id} author={author} />
         ))}
       </>
     );
