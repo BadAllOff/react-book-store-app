98ae4ec73239b92729a653ec653070ad5d0a9213
Shamil <shamil.badalloffsh@gmail.com>
2020-07-29 12:18:22 +0200

    remove: constructor from show authors btn

--- src/ShowAuthorsBtn.js
+++ src/ShowAuthorsBtn.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 class ShowAuthorsBtn extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   toggleBtn() {
     this.props.toggleShowAll();
   }
