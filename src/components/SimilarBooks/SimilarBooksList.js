eb4041467010f0001fe2d059ea62ed38065a553b
Shamil <shamil.badalloffsh@gmail.com>
2020-08-05 14:43:22 +0200

    Add: SimilarBooks component and remove books functionality

--- src/components/SimilarBooks/SimilarBooksList.js
+++ src/components/SimilarBooks/SimilarBooksList.js
@@ -2,15 +2,42 @@ import React from "react";
 import SimilarBookCard from "./SimilarBookCard";
 
 class SimilarBooksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notInterested: [],
+    };
+  }
+
+  removeBookById(id) {
+    this.setState(({ notInterested }) => {
+      notInterested: return notInterested.push(id);
+    });
+  }
+
   render() {
     const similarBooks = this.props.similarBooks;
+    const notInterested = this.state.notInterested;
+    const result = similarBooks
+      .filter((el) => -1 == notInterested.indexOf(el.id))
+      .slice(0, 3);
 
     return (
       <div>
-        {/* Ne renderit elementy. Ne mogu ponyat v 4em delo */}
-        {similarBooks.map((b) => {
-          <SimilarBookCard similarBooks={b} />;
-        })}
+        <h2>Similar books</h2>
+        <div className="row row-cols-1 row-cols-md-3">
+          {result.map((b) => {
+            return (
+              <SimilarBookCard
+                key={b.id}
+                book={b}
+                removeBookById={() => {
+                  this.removeBookById(b.id);
+                }}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
