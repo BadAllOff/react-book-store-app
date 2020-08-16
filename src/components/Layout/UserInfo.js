8403d575264ea05ea3c28fbb184ee8eabc29a82b
Shamil <shamil.badalloffsh@gmail.com>
2020-08-16 15:46:30 +0200

    Add: useContext for user section

--- src/components/Layout/UserInfo.js
+++ src/components/Layout/UserInfo.js
@@ -1,26 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../meta/AuthContext";
 
-const UserInfo = () => (
-  <AuthContext.Consumer>
-    {({ firstName, lastName, avatarUrl }) => {
-      return (
-        <ul className="navbar-nav ml-auto nav-flex-icons">
-          <li className="nav-item avatar">
-            <a className="nav-link p-0" href="#">
-            {firstName && `${firstName} ${lastName} `}
-              <img
-                src={avatarUrl}
-                className="rounded-circle z-depth-0"
-                alt={`${firstName} ${lastName}`}
-                height="35"
-              />
-            </a>
-          </li>
-        </ul>
-      );
-    }}
-  </AuthContext.Consumer>
-);
+const UserInfo = () => {
+  const u = useContext(AuthContext);
+  return (
+    <ul className="navbar-nav ml-auto nav-flex-icons">
+      <li className="nav-item avatar">
+        <a className="nav-link p-0" href="#">
+          {u.firstName && `${u.firstName} ${u.lastName} `}
+          <img
+            src={u.avatarUrl}
+            className="rounded-circle z-depth-0"
+            alt={`${u.firstName} ${u.lastName}`}
+            height="35"
+          />
+        </a>
+      </li>
+    </ul>
+  );
+};
 
 export default UserInfo;
