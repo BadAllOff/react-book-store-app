4f5a3819802d54143794ed361ac2e68eeb617969
Shamil <shamil.badalloffsh@gmail.com>
2020-08-16 15:24:21 +0200

    Remove: useBooks hook, back to HOC

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import BookList from "./components/Book/List/BookList";
 import ScrollButton from "./components/helpers/ScrollButton";
 import "./styles/styles.css";
 import Layout from "./components/Layout";
-import useBooks from "./hooks/useBooks";
 
-const App = () => {
-  const books = useBooks();
-  return (
-    <Layout>
-      {/* <SimilarBooksList similarBooks={this.props.similarBooks} /> */}
-      <BookList books={books} isLoading={!books} />
-      <ScrollButton
-        scrollStepInPx="50"
-        delayInMs="16.66"
-        ShowAtPosition={window.innerHeight / 3}
-        TransitionClassName="visible"
-      />
-    </Layout>
-  );
-};
+class App extends React.Component {
+  render() {
+    const { books } = this.props;
+    return (
+      <Layout>
+        {/* <SimilarBooksList similarBooks={this.props.similarBooks} /> */}
+        <BookList books={books} isLoading={!books} />
+        <ScrollButton
+          scrollStepInPx="50"
+          delayInMs="16.66"
+          ShowAtPosition={window.innerHeight / 3}
+          TransitionClassName="visible"
+        />
+      </Layout>
+    );
+  }
+}
 
 export default App;
