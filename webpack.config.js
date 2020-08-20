0a5f1c1655ffa05953c9a12c2c0f41475a36ca88
Shamil <shamil.badalloffsh@gmail.com>
2020-08-20 14:29:33 +0200

    add: mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "production",
@@ -14,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, use: "babel-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         use: "url-loader?limit=100000",
@@ -29,5 +30,6 @@ module.exports = {
       logo: "./src/static/favicon.svg",
       cache: true,
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
