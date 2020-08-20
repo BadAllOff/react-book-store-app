1d4aa09007874a87eb75c3924b4aa302d7e4fe67
Shamil <shamil.badalloffsh@gmail.com>
2020-08-20 14:13:21 +0200

    Add: webpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
 import "@fortawesome/fontawesome-free/css/all.css";
 import App from "./App";
-import user from "./data/user.json";
+import * as user from "./data/user.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import user from "./data/user";
 import AuthContext from "./components/meta/AuthContext";
 
 ReactDOM.render(
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: { index: "./src/index.js" },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  output: { filename: "[name].js" },
+  module: {
+    rules: [
+      { test: /\.js$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: "url-loader?limit=100000",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/static/favicon.svg",
+      cache: true,
+    }),
+  ],
+};
