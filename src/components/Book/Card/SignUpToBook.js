d0f27b9c90bab141e8fac5fbae1c99af8f45123c
Shamil <shamil.badalloffsh@gmail.com>
2020-08-24 11:39:31 +0200

    modify: add RB components to SignUpToBook

--- src/components/Book/Card/SignUpToBook.js
+++ src/components/Book/Card/SignUpToBook.js
@@ -1,35 +1,36 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import SubscriptionsTermsModal from "./SubscriptionsTermsModal";
 
 const SignUpToBook = ({ book }) => {
   return (
-    <div className="card border-dark">
-      <div className="card-body">
-        <p className="card-text">
+    <Card border="dark">
+      <Card.Body>
+        <Card.Text>
           Sign up and get latest information about "{book.title}"{" "}
-        </p>
-        <form>
-          <div className="form-group row">
-            <div className="col-sm-12">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="form-group row">
-            <div className="col-sm-12">
-              <button type="submit" className="btn btn-primary btn-block">
-                Sign up
-              </button>
-              <SubscriptionsTermsModal />
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+        </Card.Text>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              type="email"
+              className="form-control"
+              placeholder="Email"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button  variant="primary" size="lg" block >
+              Sign up
+            </Button>
+          </Form.Group>
+        </Form>
+        <Card.Text>
+          <SubscriptionsTermsModal />
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
