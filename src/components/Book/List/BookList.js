29dbd718dc6ba2dc7e4e8672e61b97384b022a67
Shamil <shamil.badalloffsh@gmail.com>
2020-09-06 14:04:52 +0200

    Modify: moved HOC folder into Components folder

--- src/components/Book/List/BookList.js
+++ src/components/Book/List/BookList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ShortBookCard from "../Card/ShortBookCard";
-import withBooks from "../../../HOC/withBooks";
-import withLoader from "../../../HOC/withLoader";
+import withBooks from "../../HOC/withBooks";
+import withLoader from "../../HOC/withLoader";
 import { CardColumns, CardGroup, CardDeck } from "react-bootstrap";
 
 const BookList = ({ books }) => {
