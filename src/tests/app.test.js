bd2949658877ceecb229ee54af8d3e2bd32ec181
Shamil <shamil.badalloffsh@gmail.com>
2020-09-24 17:24:17 +0200

    add: test main and wish list page rendering

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 
@@ -16,5 +16,34 @@ test("landing on a bad page shows 404 page", () => {
 
   expect(
     findByText(`Sorry, we didn't find anything by this address`)
+  ).toBeTruthy();
+});
+
+
+test("landing on a main page", () => {
+  const history = createMemoryHistory();
+  history.push("/");
+  const { findByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  expect(
+    findByText(`Keep reading...`)
+  ).toBeTruthy();
+});
+
+test("landing on a wish list page", () => {
+  const history = createMemoryHistory();
+  history.push("/");
+  const { findByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  expect(
+    findByText(`My wish list`)
   ).toBeTruthy();
 });
